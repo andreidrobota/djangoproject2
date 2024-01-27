29eb05f3eba74c01d5fa4422309d4a5ad7f4d10c
andreidrobota <drobotaandrei@gmail.com>
2024-01-27 09:43:27 +0000

    Add administration interface

--- codestar/settings.py
+++ codestar/settings.py
@@ -89,6 +89,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://8000-andreidrobo-djangoproje-qbla4qy2bak.ws-eu107.gitpod.io",
+    "https://*.herokuapp.com"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators

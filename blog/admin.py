94bec00cb8dafab742f56f89383cb13f53d41bcd
andreidrobota <drobotaandrei@gmail.com>
2024-01-28 08:38:02 +0000

    enable serving of static files

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from .models import Post, Comment
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_delay = ('title', 'slug', 'status')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+    list_display = ('title', 'slug', 'status', 'created_on')
 
 
 # Register your models here.
-admin.site.register(Post)
 admin.site.register(Comment)

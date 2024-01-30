fd9cb3d71aea9db7d563b624751d443543f03f6a
andreidrobota <drobotaandrei@gmail.com>
2024-01-30 07:01:12 +0000

    added post_detail view

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -9,3 +9,20 @@ class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1)
     template_name = "blog/index.html"
     paginate_by = 6
+
+
+def post_detail(request, slug):
+    """
+    Siplay and individual :model: 'blog.Post',
+
+    **context**
+
+    ``post``
+        an instance of :model:`blog.Post`.
+
+    **template**
+        :template:`blog/post_detail.html`
+    """
+    queryset = Post.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+    return reder(request, "blog/post_detail.html", {"post": post},)

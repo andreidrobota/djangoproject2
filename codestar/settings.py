8f7b086ec67b60703ea6ab482a59ea56c3636fe8
andreidrobota <drobotaandrei@gmail.com>
2024-01-31 07:18:56 +0000

    Fix 500 server error

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-andreidrobo-djangoproje-qbla4qy2bak.ws-eu107.gitpod.io',
                     '.herokuapp.com']
